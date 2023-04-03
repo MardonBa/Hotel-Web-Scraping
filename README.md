0ddc815a4e2f456ff0581023defa9d59b0ce0846
Bayan Mardon <mardon.junior@gmail.com>
2023-04-03 07:43:43 -0700

    add setup code and ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hotel-Web-Scraping
+
+Web-Sraping program used to find hotel room prices in the Los Altos area based on various criteria
---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
