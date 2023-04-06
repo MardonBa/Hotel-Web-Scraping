6cecc22706962ff1a91eff029c277e599581af34
Bayan Mardon <mardon.junior@gmail.com>
2023-04-05 21:32:41 -0700

    Add error exceptions

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ import time
 
 # Selenium import and setup code
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 
-service = ChromeService(executable_path=ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
 # Initiates the web driver
 # Options = options is to hide the browser, not necessary until final product
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome()
 
 # Loads the Marriott link the the browser
 driver.get("https://www.marriott.com/default.mi")
@@ -36,77 +33,120 @@ submit_button.click()
 ## Saving webpages to their respective variables using driver.page_source
 ## Then creating a BeautifulSoup instance title webpagename_soup
 
-## Residence Inn Palo Alto Mountain Viewx
-## Navigates to the page with rooms for this hotel
-driver.implicitly_wait(1)
-submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SFOMV"]/div/div[2]/div/div/a')
-submit_button.click()
+## Residence Inn Palo Alto Mountain View
+while True:
+    ## Navigates to the page with rooms for this hotel
+    try:
+        driver.implicitly_wait(1)
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SFOMV"]/div/div[2]/div/div/a')
+        submit_button.click()
 
-residence_inn_palo_alto_mountain_view = driver.page_source
-residence_inn_palo_alto_mountain_view_soup = BeautifulSoup(residence_inn_palo_alto_mountain_view, "html.parser")
+        residence_inn_palo_alto_mountain_view = driver.page_source
+        residence_inn_palo_alto_mountain_view_soup = BeautifulSoup(residence_inn_palo_alto_mountain_view, "html.parser")
 
-## Goes back to the page with hotels on it
-driver.back() 
+        ## Goes back to the page with hotels on it
+        driver.back() 
+        break
+
+    except:
+        print("error finding this element by XPATH: ", "Residence Inn Palo Alto Mountain View")
+        break
 
 
 ## Residence Inn Palo Alto Los Altos
-## Navigates to the page with rooms for this hotel
-driver.implicitly_wait(1)
-submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-PAORI"]/div/div[2]/div/div/a')
-submit_button.click()
+while True:
+    ## Navigates to the page with rooms for this hotel
+    try:
+        driver.implicitly_wait(1)
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-PAORI"]/div/div[2]/div/div/a')
+        submit_button.click()
 
-residence_inn_palo_alto_los_altos = driver.page_source
-residence_inn_palo_alto_los_altos_soup = BeautifulSoup(residence_inn_palo_alto_los_altos, "html.parser")
+        residence_inn_palo_alto_los_altos = driver.page_source
+        residence_inn_palo_alto_los_altos_soup = BeautifulSoup(residence_inn_palo_alto_los_altos, "html.parser")
 
-## Goes back to the page with hotels on it
-driver.back()
+        ## Goes back to the page with hotels on it
+        driver.back()
+        break
+
+    except:
+        print("error finding this element by XPATH: ", "Residence Inn Palo Alto Los Altos")
+        break
 
 
 ## Courtyard Palo Alto Los Altos
-## Navigate to the page with rooms for this hotel
-driver.implicitly_wait(1)
-submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-PAOCY"]/div/div[2]/div/div/a')
-submit_button.click()
+while True:
+    ## Navigate to the page with rooms for this hotel
+    try:
+        driver.implicitly_wait(1)
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-PAOCY"]/div/div[2]/div/div/a')
+        submit_button.click()
 
-courtyard_palo_alto_los_altos = driver.page_source
-courtyard_palo_alto_los_altos_soup = BeautifulSoup(courtyard_palo_alto_los_altos, "html.parser")
+        courtyard_palo_alto_los_altos = driver.page_source
+        courtyard_palo_alto_los_altos_soup = BeautifulSoup(courtyard_palo_alto_los_altos, "html.parser")
 
-## Goes back to the page with the hotels on it
-driver.back()
+        ## Goes back to the page with the hotels on it
+        driver.back()
+        break
+
+    except:
+        print("error finding this element by XPATH: ", "Courtyard Palo Alto Los Altos")
+        break
 
 
 ## AC Hotel Palo Alto
-## Navigate to the page with rooms for this hotel
-driver.implicitly_wait(1)
-submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SJCAO"]/div/div[2]/div/div/a')
-submit_button.click()
+while True:
+    try:
+        ## Navigate to the page with rooms for this hotel
+        driver.implicitly_wait(1)
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SJCAO"]/div/div[2]/div/div/a')
+        submit_button.click()
 
-ac_hotel_palo_alto = driver.page_source
-ac_hotel_palo_alto_soup = BeautifulSoup(ac_hotel_palo_alto, "html.parser")
+        ac_hotel_palo_alto = driver.page_source
+        ac_hotel_palo_alto_soup = BeautifulSoup(ac_hotel_palo_alto, "html.parser")
 
-## Goes back to the page with the hotels on it
-driver.back()
+        ## Goes back to the page with the hotels on it
+        driver.back()
+        break
+    
+    except: 
+        print("error finding this element by XPATH: ", "AC Hotel Palo Alto")
+        break
 
 
 ## Hotel Citrine Palo Alto, a Tribute Portfolio Hotel
-## Navigate to the page with the rooms for this hotel
-driver.implicitly_wait(1)
-submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SJCTX"]/div/div[2]/div/div/a')
-submit_button.click()
+while True:
+    ## Navigate to the page with the rooms for this hotel
+    try:
+        driver.implicitly_wait(1)
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SJCTX"]/div/div[2]/div/div/a')
+        submit_button.click()
 
-hotel_citrine_palo_alto = driver.page_source
-hotel_citrine_palo_alto_soup = BeautifulSoup(hotel_citrine_palo_alto, "html.parser")
+        hotel_citrine_palo_alto = driver.page_source
+        hotel_citrine_palo_alto_soup = BeautifulSoup(hotel_citrine_palo_alto, "html.parser")
 
-## Goes back to the page with the hotels on it
-driver.back()
+        ## Goes back to the page with the hotels on it
+        driver.back()
+        break
+    
+    except:
+        print("error finding this element by XPATH: ", "Hotel Citrine Palo Alto")
+        break
 
 
 ## Aloft Mountain View
-## Navigate to the page with thr rooms for this hotel
-driver.implicitly_wait(1)
-submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SJCAM"]/div/div[2]/div/div/a')
-submit_button.click()
-time.sleep(5)
+while True:
+    ## Navigate to the page with the rooms for this hotel
+    try:
+        driver.implicitly_wait(1)
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="property-record-map-SJCAM"]/div/div[2]/div/div/a')
+        submit_button.click()
+        time.sleep(5)
 
-aloft_mountain_view = driver.page_source
-aloft_mountain_view_soup = BeautifulSoup(aloft_mountain_view, "html.parser")
+        aloft_mountain_view = driver.page_source
+        aloft_mountain_view_soup = BeautifulSoup(aloft_mountain_view, "html.parser")
+        
+        break
+    
+    except:
+        print("error finding this element by XPATH: ", "Aloft Mountain View")
+        break
